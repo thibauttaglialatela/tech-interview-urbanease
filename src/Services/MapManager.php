e580eb67d016ac09bf1d03e1884b5ab56ead230e
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-12-02 10:18:46 +0100

    create getRandomIsland

--- src/Services/MapManager.php
+++ src/Services/MapManager.php
@@ -1,21 +1,36 @@
 <?php
+
 namespace App\Services;
 
 use App\Entity\Tile;
+use App\Repository\TileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MapManager extends AbstractController
 {
-    
+
 
     public function tileExists(int $x, int $y): bool
     {
         $tile = $this->getDoctrine()->getRepository(Tile::class)->findOneBy(['coordX' => $x, 'coordY' => $y]);
-        
+
         if ($tile) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public function getRandomIsland(TileRepository $tileRepository)
+    {
+        $tiles = $tileRepository->findAll();
+        $islandArray = [];
+        foreach ($tiles as $tile) {
+            if ($tile->getType() === 'island') {
+                array_push($islandArray, $tile);
+            }
+        }
+
+        return array_rand($islandArray);
     }
 }
